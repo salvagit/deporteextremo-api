157aef027e5d9fdb1d22b1f0a28b385bc26c8ca2
Gustavo Villada <villada@gmail.com>
2019-11-26 21:30:34 -0300

    listing notas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
 const express = require("express");
+const cors = require("cors");
+const loki = require("lokijs");
 
+
+const db = new loki("./notas.json");
 const app = express();
 
-app.get("/api/v1/notas", (req, res) => {
-  res.status(200).send({
+let notas = db.addCollection("notas");
+
+
+db.loadDatabase({}, () => {
+  notas = db.getCollection("notas");
+});
+
+app.use(cors());
+
+
+app.get("/api/v1/notas:id?", (req, res) => {
+  let status = 200;
+  let response;
+  try{
+    response = notas.data;
+    if( req.params.id){
+      response = notas
+        .chain()
+        .find({$loki: Number(reqs.params.id)})
+        .data();
+    }
+  }catch(e){
+    status = 500;
+  }
+  res.status(status).send({
     success: true,
-    message: "get notas"
+    message: "GET NOTAS",
+    response
   });
 });
 
